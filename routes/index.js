d153f6243de562988adbc843bcb0eaf6406755a9
Sankruth Kota <skota121@users.noreply.github.com>
2016-11-20 07:45:29 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 'use strict';
+var bodyParser = require('body-parser');
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
@@ -7,5 +12,26 @@ module.exports = function(app) {
 
     app.get('/data', function(req, res) {
         res.render('pages/data');
+    });
+
+    app.use(bodyParser.urlencoded({
+    	extended: true
+    }));
+
+    app.use(bodyParser.text())
+
+    app.post('/data', function(req, res) {
+        res.render('pages/data');
+        // do something with the datum
+        console.log(req.body)
+
+
+        var htmlsource = fs.readFileSync('data.ejs','utf-8');
+        var $ = window.$
+        var title = $("title").text()
+        $('#sanqrooth').text(title);
+
+        console.log(documentToSource(window.document))
+
     });
 };
