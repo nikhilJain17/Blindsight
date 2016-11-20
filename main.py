f741882a4c9323712d97220e09e8fdf596fc7bec
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-11-19 22:36:51 -0500

    step one complete: phoen plus aderesse

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ api = None
 
 # send the auth tokens and whatnot
 # def auth ():
-# 	auth = tweepy.OAuthHandler('3vak8cyOxT0dHvbYvqUZ7i6Hp', 'tACDJJL56ZQNWTSHsUN2bYYn7qsuxRjFNvpVvfNYMHocAZqRdl')
-# 	auth.set_access_token('1581800299-aiZuhraRWWWacTCegHpMFiUIPJDv1q2uv6x3udE', '47jzAZr4CqrZhvjsTY2GLwW750NC4qrJ16jATauUgrnL2')
 
 # 	api = tweepy.API(auth)
 
