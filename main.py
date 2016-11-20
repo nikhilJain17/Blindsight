203f51d6ef9a6f00cf0b05649d0a8dcad9aff770
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-11-19 23:53:27 -0500

    calculating followers of followers

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import tweepy
 import json
+import oauth2
 
 # initialize the api object to be null
 api = None
 
-# send the auth tokens and whatnot
-# def auth ():
+def oauth_req(url, key, secret, http_method="GET", post_body="", http_headers=None):
+    consumer = oauth2.Consumer(key='3vak8cyOxT0dHvbYvqUZ7i6Hp', secret='tACDJJL56ZQNWTSHsUN2bYYn7qsuxRjFNvpVvfNYMHocAZqRdl')
+    token = oauth2.Token(key=key, secret=secret)
+    client = oauth2.Client(consumer, token)
+    resp, content = client.request( url, method=http_method, body=post_body, headers=http_headers )
+    return content
 
-# 	api = tweepy.API(auth)
-
-# 	print "done"
 
 
 if __name__ == '__main__':
 	# authorize the api
-	auth = tweepy.OAuthHandler('"stop looking"')
-	auth.set_access_token('DONT LOOK HERE PLEASE')
+	auth = tweepy.OAuthHandler('tomaty tomato')
 
 	api = tweepy.API(auth)
 	
@@ -31,7 +32,22 @@ if __name__ == '__main__':
 	usr = "skota121"
 	
 	phone_occurences = stringified.count(usr) / 5
-	print "phone occurences", phone_occurences
+	print "phone occurences: ", phone_occurences
+
+	#2. get followers of followers
+	allfriends = oauth_req('https://api.twitter.com/1.1/followers/list.json?cursor=-1&screen_name=skota121&skip_status=true&include_user_entities=false', '1581800299-aiZuhraRWWWacTCegHpMFiUIPJDv1q2uv6x3udE', '47jzAZr4CqrZhvjsTY2GLwW750NC4qrJ16jATauUgrnL2' )
+	allfriends = json.loads(allfriends)
+
+	followers_of_followers = 0;
+
+	# iterate through each follower
+	for user in allfriends['users']:
+		followers_of_followers += user['followers_count']
+
+	print "followers of followers: ", followers_of_followers
+
+
+
 
 
 
