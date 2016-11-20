dc48bc470f594dacd9572786a7a86730df00ce0a
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-11-20 00:51:07 -0500

    pupdate

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def oauth_req(url, key, secret, http_method="GET", post_body="", http_headers=No
 
 if __name__ == '__main__':
 	# authorize the api
-	auth = tweepy.OAuthHandler('tomaty tomato')
+	auth = tweepy.OAuthHandler('3vak8cyOxT0dHvbYvqUZ7i6Hp', 'tACDJJL56ZQNWTSHsUN2bYYn7qsuxRjFNvpVvfNYMHocAZqRdl')
+	auth.set_access_token('1581800299-aiZuhraRWWWacTCegHpMFiUIPJDv1q2uv6x3udE', '47jzAZr4CqrZhvjsTY2GLwW750NC4qrJ16jATauUgrnL2')
 
 	api = tweepy.API(auth)
 	
@@ -45,6 +46,21 @@ if __name__ == '__main__':
 		followers_of_followers += user['followers_count']
 
 	print "followers of followers: ", followers_of_followers
+
+	#3. get how many people you are blocking
+	blocked_people = oauth_req('https://api.twitter.com/1.1/blocks/ids.json?stringify_ids=true&cursor=-1&screen_name=skota121&skip_status=true&include_user_entities=false', '1581800299-aiZuhraRWWWacTCegHpMFiUIPJDv1q2uv6x3udE', '47jzAZr4CqrZhvjsTY2GLwW750NC4qrJ16jATauUgrnL2' )
+	blocked_people = json.loads(blocked_people)
+
+	num_of_blocked_folks = len(blocked_people['ids'])
+
+	print "blocked people: ", num_of_blocked_folks
+
+	#4. how much you posted per week
+	recent_tweets = oauth_req('https://api.twitter.com/1.1/search/tweets.json?q=@skota121&result_type=recent', '1581800299-aiZuhraRWWWacTCegHpMFiUIPJDv1q2uv6x3udE', '47jzAZr4CqrZhvjsTY2GLwW750NC4qrJ16jATauUgrnL2')
+	print recent_tweets
+
+
+
 
 
 
